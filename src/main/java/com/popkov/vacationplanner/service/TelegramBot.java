3cd14b7319c8e4a3e6c7e010ec7c482996c38383
Popkov <ivan.popkov.ufa@gmail.com>
2023-07-17 16:44:38 +0500

    initial

---
+++ src/main/java/com/popkov/vacationplanner/service/TelegramBot.java
@@ -0,0 +1,55 @@
+package com.popkov.vacationplanner.service;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import com.popkov.vacationplanner.config.BotConfig;
+
+
+
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+
+    }
+
+    private void sendMessage(long chatId, String stringToSend) {
+        SendMessage message = new SendMessage(String.valueOf(chatId), stringToSend);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            switch (messageText) {
+                case "/start":
+                    sendMessage(chatId, "Hi, " + update.getMessage().getChat().getFirstName() + ", nice to meet you!");
+                    break;
+                default:
+                    sendMessage(chatId, "Sorry, command was not recognized:(");
+            }
+        }
+    }
+}
---
+++ src/test/java/com/popkov/vacationplanner/VacationPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popkov.vacationplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
